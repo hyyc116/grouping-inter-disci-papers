ee1fa435ac7cbc327a330b9c2c73da1d1426ce85
hyyc116 <hyyc116@gmail.com>
2021-04-05 18:31:34 +0800

    add

---
+++ src/MAG_field_ITR.py
@@ -0,0 +1,30 @@
+#coding:utf-8
+'''
+1. 学科相似性计算的结果
+2. 转化率与I0的关系
+3. 转化率函数拟合：转化率与学科相似性之间的关系
+4. 通过转化函数计算各个领域的转化区间
+
+'''
+from basic_config import *
+
+
+## 学科相似度分布
+def plot_topic_rel():
+    fos1_fos2_func = json.loads(open('data/fos1_fos2_func.json').read())
+    rels = ['t1,t2,rel']
+    for t1 in fos1_fos2_func.keys():
+        for t2 in fos1_fos2_func[t1].keys():
+            num = fos1_fos2_func[t1].get(t2, 0)
+            rels.append('{:},{:},{:}'.format(t1, t2, num))
+
+    open('data/mag_topic_relevance.csv', 'w').write('\n'.join(rels))
+    logging.info('topic relevance saved to data/topic_relevance.csv')
+
+    ## 画热力图
+    plot_heatmap('data/topic_relevance.csv', 'correlation matrix', 'field',
+                 'field', 'fig/mag_topic_rel_matrix.png')
+
+
+if __name__ == '__main__':
+    plot_topic_rel()

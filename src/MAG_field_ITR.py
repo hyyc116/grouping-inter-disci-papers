129e4f54a734c5adf7ae979a202c686d90d6c188
hyyc116 <hyyc116@gmail.com>
2021-04-23 15:49:22 +0800

    Update MAG_field_ITR.py

--- src/MAG_field_ITR.py
+++ src/MAG_field_ITR.py
@@ -41,5 +41,49 @@ def plot_topic_rel():
                  'field', 'fig/mag_topic_rel_matrix.png')
 
 
+#转化率与学科之间的关系
+def cor_sim_itr():
+    # 两个学科的相似度
+    fos1_fos2_func = json.loads(open('data/fos1_fos2_func.json').read())
+    # 相似度与转化率的关系
+
+    fos1_fos2_itrs = defaultdict(lambda: defaultdict(list))
+    for line in open('data/paper_ITR.csv'):
+
+        line = line.strip()
+
+        if line.startswith('pid'):
+            continue
+
+        pid, subj, osubj, func, I0, IT, ITR = line.split(',')
+
+        fos1_fos2_itrs[subj][osubj].append(ITR)
+
+    xs = []
+    ys = []
+    for fos1 in fos1_fos2_itrs.keys():
+
+        for fos2 in fos1_fos2_itrs[fos1].keys():
+
+            xs.append(fos1_fos2_func[fos1][fos2])
+            ys.append(np.mean(fos1_fos2_itrs[fos1][fos2]))
+
+    plt.figure(figsize=(5, 4))
+
+    plt.plot(xs, ys, 'o', fill_style='none')
+
+    plt.xscale("log")
+    plt.yscale('log')
+
+    plt.xlabel('field similiraty')
+    plt.ylabel('ITR')
+
+    plt.tight_layout()
+
+    plt.savefig('fig/sim_ITR.png', dpi=400)
+
+    logging.info('fig saved to fig/sim_ITR.png')
+
+
 if __name__ == '__main__':
     plot_topic_rel()

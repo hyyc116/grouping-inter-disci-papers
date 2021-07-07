a04a2e7b9fa6e9ab7a3cdf2415ec72da411bdfea
hyyc116 <hyyc116@gmail.com>
2021-07-07 17:39:27 +0800

    Update plot_label_dis.py

--- src/plot_label_dis.py
+++ src/plot_label_dis.py
@@ -279,7 +279,7 @@ def survey(results, category_names):
 
         r, g, b, _ = color
         text_color = 'white' if r * g * b < 0.5 else 'darkgrey'
-        ax.bar_label(rects, label_type='center', color=text_color)
+        ax.bar_label(rects, label_type='center', color=text_color, fmt='%.2f')
     ax.legend(ncol=len(category_names),
               bbox_to_anchor=(0, 1),
               loc='low left',

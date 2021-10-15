5794432b528c61bdc2e4f11b5e3034dc80020ebf
hyyc116 <hyyc116@gmail.com>
2021-10-15 11:35:23 +0800

    Update MAG_field_ITR.py

--- src/MAG_field_ITR.py
+++ src/MAG_field_ITR.py
@@ -166,7 +166,7 @@ def cor_sim_itr():
 
     print(res.summary())
     predict_v = res.predict(exog=dict(FS=xss))
-    print(r2_score(yss,np.exp(predict_v)))
+    print(r2_score(np.log(yss),predict_v))
 
 
     prstd, iv_l, iv_u = wls_prediction_std(res)

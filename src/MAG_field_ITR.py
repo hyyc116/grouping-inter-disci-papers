adfa21ab4bb2a19277fbaaa514fce6c7024d7b2d
hyyc116 <hyyc116@gmail.com>
2021-10-15 11:01:28 +0800

    Update MAG_field_ITR.py

--- src/MAG_field_ITR.py
+++ src/MAG_field_ITR.py
@@ -150,13 +150,13 @@ def cor_sim_itr():
     # plt.plot(xs, ys, 'o')
     ax = sns.scatterplot(data=data, x='FS', y='ITR')
 
-    sns.lineplot(
-        data=data, x="FS", y="ITR",ax=ax,alpha=0.5
-    )
+    # sns.lineplot(
+    #     data=data, x="FS", y="ITR",ax=ax,alpha=0.5
+    # )
 
-    xs, ys = zip(*lowess(data['ITR'], data['FS'], frac=1. / 3, it=0))
+    xss, yss = zip(*lowess(data['ITR'], data['FS'], frac=1. / 3, it=0))
 
-    plt.plot(xs, ys, '--', c='r')
+    plt.plot(xss, yss, '--', c='r',alpha=0.5)
 
     mod = smf.ols(formula='np.log(ITR) ~ np.log(FS)', data=data)
 
